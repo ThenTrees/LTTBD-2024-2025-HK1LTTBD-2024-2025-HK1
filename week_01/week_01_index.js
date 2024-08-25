448ae4b84dbc410ffa3ffbd6332a91a38f7fbd54
thentrees <thientri.trank17@gmail.com>
2024-08-25 17:16:49 +0700

    Lab_01: 50_challebges_newbie_part_01

--- week_01/week_01_index.js
+++ week_01/week_01_index.js
@@ -76,3 +76,95 @@ const averageArray = (numbers) => {
 const getPositiveNumbers = (numbers) => {
     document.write(numbers.filter((number) => number > 0));
 };
+
+//Find the maximum number in an array of numbersFind the maximum number in an array of numbers
+function findMaxNumber(arr) {
+    return Math.max(...arr);
+}
+
+//Print the first 10 Fibonacci numbers without recursion javascript
+function printFirst10Fibonacci() {
+    let a = 0,
+        b = 1;
+    for (let i = 0; i < 10; i++) {
+        document.write(a);
+        [a, b] = [b, a + b];
+    }
+}
+
+function fibonacci(n) {
+    if (n <= 1) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+function isPrime(num) {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+    if (num % 2 === 0 || num % 3 === 0) return false;
+    for (let i = 5; i * i <= num; i += 6) {
+        if (num % i === 0 || num % (i + 2) === 0) return false;
+    }
+    return true;
+}
+
+function sumOfDigits(num) {
+    return num
+        .toString()
+        .split("")
+        .reduce((sum, digit) => sum + parseInt(digit), 0);
+}
+
+function printFirst100Primes() {
+    let count = 0;
+    let num = 2;
+    while (count < 100) {
+        if (isPrime(num)) {
+            document.write(num);
+            count++;
+        }
+        num++;
+    }
+}
+
+function primesGreaterThanN(n, p) {
+    let primes = [];
+    let num = n + 1;
+    while (primes.length < p) {
+        if (isPrime(num)) {
+            primes.push(num);
+        }
+        num++;
+    }
+    return primes;
+}
+
+function rotateLeft(arr) {
+    if (arr.length === 0) return arr;
+    let first = arr.shift();
+    arr.push(first);
+    return arr;
+}
+function rotateRight(arr) {
+    if (arr.length === 0) return arr;
+    let last = arr.pop();
+    arr.unshift(last);
+    return arr;
+}
+function reverseArray(arr) {
+    return arr.reverse();
+}
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+function mergeArrays(arr1, arr2) {
+    return arr1.concat(arr2);
+}
+function symmetricDifference(arr1, arr2) {
+    return arr1
+        .filter((item) => !arr2.includes(item))
+        .concat(arr2.filter((item) => !arr1.includes(item)));
+}
+
+function difference(arr1, arr2) {
+    return arr1.filter((item) => !arr2.includes(item));
+}
